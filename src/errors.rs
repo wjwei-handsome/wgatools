962caa102a18126a21231b369f6a4590aa36f664
wjwei-handsome <56633839+wjwei-handsome@users.noreply.github.com>
2023-07-05 00:38:30 +0800

    define some err

--- src/errors.rs
+++ src/errors.rs
@@ -4,7 +4,7 @@ use std::fmt::Formatter;
 use std::{fmt, io};
 
 /// Enum the file types
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum FileFormat {
     Maf,
     Sam,
@@ -32,6 +32,10 @@ pub enum ParseErrorKind {
     Io,
     /// Empty file/stream
     Empty,
+    /// Serde error when deserialize
+    Serde,
+    /// CIGAR parse error
+    ParseCigar,
 }
 
 ///The parse error returns
@@ -45,6 +49,19 @@ pub struct ParseError {
     pub file_info: FileInfo,
 }
 
+impl ParseError {
+    pub fn new_parse_cigar_err(format: FileFormat) -> Self {
+        ParseError {
+            msg: String::from("Parse cigar error, please check the format."),
+            kind: ParseErrorKind::ParseCigar,
+            file_info: FileInfo {
+                name: String::from(""),
+                format,
+            },
+        }
+    }
+}
+
 impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self.kind {
@@ -54,14 +71,21 @@ impl fmt::Display for ParseError {
                     "IO error: {}, check path: {}",
                     self.msg, self.file_info.name
                 )
-            }
+            },
             ParseErrorKind::Empty => {
                 write!(
                     f,
                     "Empty input: {}, check path: {}",
                     self.msg, self.file_info.name
                 )
-            }
+            },
+            _ => {
+                write!(
+                    f,
+                    "Parse error: {}, check path: {}",
+                    self.msg, self.file_info.name
+                )
+            },
         }
     }
 }
@@ -71,6 +95,19 @@ impl From<io::Error> for ParseError {
         ParseError {
             msg: err.to_string(),
             kind: ParseErrorKind::Io,
+            file_info: FileInfo {
+                name: String::new(),
+                format: FileFormat::Unknown,
+            },
+        }
+    }
+}
+
+impl From<csv::Error> for ParseError {
+    fn from(err: csv::Error) -> ParseError {
+        ParseError {
+            msg: err.to_string(),
+            kind: ParseErrorKind::Serde,
             file_info: FileInfo {
                 name: String::new(),
                 format: FileFormat::Unknown,
