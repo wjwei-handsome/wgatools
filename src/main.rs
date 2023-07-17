c69c04449dbc21f59d0d508ef683fcbff9d5aa69
wjwei-handsome <56633839+wjwei-handsome@users.noreply.github.com>
2023-07-18 00:53:14 +0800

    add a case;

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use wgalib::parser::common::{AlignRecord, FileFormat};
-use wgalib::parser::paf::PAFReader;
+use wgalib::parser::common::FileFormat;
+// use wgalib::parser::paf::PAFReader;
 use wgalib::parser::maf::MAFReader;
 
 fn main() {
@@ -11,10 +11,14 @@ fn main() {
 
     let mut reader = MAFReader::from_path("data/output/test.maf").unwrap();
     let _header = &reader.header;
-    for record in reader.records() {
-        // let record = record.unwrap();
-        // println!("{:?}", record);
-        println!("{}", record.unwrap().get_cigar_string());
-    }
+    // for record in reader.records() {
+    //     let record = record.unwrap();
+    //     // println!("{:?}", record);
+    //     println!("{}\nquery_start:{}\tquery_end:{}\ntarget_start:{}\ttarget_end:{}",
+    //              record.get_cigar_string(),
+    //              record.query_start(),record.query_end(),
+    //     record.target_start(),record.target_end());
+    // }
+    reader.convert("test.paf", FileFormat::Paf);
     // println!("{:?}", reader.inner.buffer()); // buffer has been consumed
 }
