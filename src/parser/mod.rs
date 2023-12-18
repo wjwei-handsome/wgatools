749ef54b5ac76522e5e1c53bbd156f0da508eb7d
wjwei-handsome <56633839+wjwei-handsome@users.noreply.github.com>
2023-12-18 22:57:17 +0800

    manage parser module

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,12 +1,5 @@
 pub mod chain;
-pub(crate) mod cigar;
+pub mod cigar;
 pub mod common;
 pub mod maf;
 pub mod paf;
-
-// pub use chain::ChainDataLine;
-// pub use chain::Header;
-// pub use cigar::parse_cigar_to_blocks;
-// pub use cigar::parse_cigar_to_chain;
-// pub use paf::PafReader;
-// TODO: manage public modules
