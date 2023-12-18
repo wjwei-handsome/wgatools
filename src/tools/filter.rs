2845dd913aa62681f368daa8dda8c358c55ac329
wjwei-handsome <56633839+wjwei-handsome@users.noreply.github.com>
2023-12-18 22:54:20 +0800

    Refactor filter error handling

--- src/tools/filter.rs
+++ src/tools/filter.rs
@@ -1,12 +1,32 @@
-use crate::parser::{
-    common::AlignRecord,
-    maf::{MAFReader, MAFWriter},
-    paf::PAFReader,
+use crate::{
+    errors::WGAError,
+    parser::{
+        chain::ChainReader,
+        common::AlignRecord,
+        maf::{MAFReader, MAFWriter},
+        paf::PAFReader,
+    },
 };
-use std::{
-    error::Error,
-    io::{Read, Write},
-};
+use std::io::{Read, Write};
+
+// filter chain
+pub fn filter_chain<R: Read + Send>(
+    mut reader: ChainReader<R>,
+    _writer: &mut dyn Write,
+    min_block_size: u64,
+    min_query_size: u64,
+) -> Result<(), WGAError> {
+    for rec in reader.records() {
+        println!("www");
+        let rec = rec?;
+        let rec = filter_alignrec(&rec, min_block_size, min_query_size)?;
+        // just write the record
+        if let Some(rec) = rec {
+            println!("{:?}", rec)
+        }
+    }
+    Ok(())
+}
 
 // filter paf
 pub fn filter_paf<R: Read + Send>(
@@ -14,18 +34,13 @@ pub fn filter_paf<R: Read + Send>(
     writer: &mut dyn Write,
     min_block_size: u64,
     min_query_size: u64,
-) -> Result<(), Box<dyn Error>> {
+) -> Result<(), WGAError> {
     let mut pafwtr = csv::WriterBuilder::new()
         .delimiter(b'\t')
         .has_headers(false)
         .from_writer(writer);
     for rec in reader.records() {
-        let rec = match rec {
-            Ok(r) => r,
-            Err(e) => {
-                return Err(Box::new(e));
-            }
-        };
+        let rec = rec?;
         let rec = filter_alignrec(&rec, min_block_size, min_query_size)?;
         // just write the record
         if let Some(rec) = rec {
@@ -41,22 +56,21 @@ pub fn filter_maf<R: Read + Send>(
     writer: &mut dyn Write,
     min_block_size: u64,
     min_query_size: u64,
-) -> Result<(), Box<dyn Error>> {
+) -> Result<(), WGAError> {
     // init a MAFWriter
     let mut mafwtr = MAFWriter::new(writer);
     // write header
-    mafwtr.write_header("#maf version=1.6 filter=..".to_owned());
+    let header = format!(
+        "#maf version=1.6 filter=blocksize>={} querysize>={}",
+        min_block_size, min_query_size
+    );
+    mafwtr.write_header(header)?;
     for rec in reader.records() {
-        let rec = match rec {
-            Ok(r) => r,
-            Err(e) => {
-                return Err(Box::new(e));
-            }
-        };
+        let rec = rec?;
         let rec = filter_alignrec(&rec, min_block_size, min_query_size)?;
         // just write the record
         if let Some(rec) = rec {
-            mafwtr.write_record(rec);
+            mafwtr.write_record(rec)?;
         }
     }
     Ok(())
@@ -67,10 +81,11 @@ fn filter_alignrec<T: AlignRecord>(
     rec: &T,
     min_block_size: u64,
     min_query_size: u64,
-) -> Result<Option<&T>, Box<dyn Error>> {
+) -> Result<Option<&T>, WGAError> {
     let query_length = rec.query_length();
     let block_length = rec.target_align_size();
 
+    // if in condition, return None
     if (block_length < min_block_size) | (query_length < min_query_size) {
         return Ok(None);
     }
