45ea30209043e22a904365c164d2e580faa925c6
wjwei-handsome <56633839+wjwei-handsome@users.noreply.github.com>
2024-03-21 14:46:40 +0800

    impl `chunk`

---
+++ src/tools/chunk.rs
@@ -0,0 +1,68 @@
+use crate::{
+    errors::WGAError,
+    parser::{
+        common::recount_align_size,
+        maf::{MAFReader, MAFRecord, MAFSLine, MAFWriter},
+    },
+};
+use std::io::{Read, Write};
+
+// mian function of chunk maf
+pub fn chunk_maf<R: Read + Send>(
+    mut reader: MAFReader<R>,
+    chunk_length: u64,
+    writer: &mut dyn Write,
+) -> Result<(), WGAError> {
+    // init a MAFWriter
+    let mut mafwtr = MAFWriter::new(writer);
+    // write header
+    let header = format!("#maf version=1.6 split_length={}", chunk_length);
+    mafwtr.write_header(header)?;
+
+    // chunk each block
+    for rec in reader.records() {
+        let rec = rec?;
+        let block_length = rec.slines[0].seq.len() as u64;
+        let mut chunk_start = 0;
+        let mut chunk_end = chunk_length;
+
+        // chunk each s-line in a block until the end
+        while chunk_end < block_length {
+            let new_rec = emit_new_maf_rec(&rec, chunk_start, chunk_end)?;
+            mafwtr.write_record(&new_rec)?;
+            chunk_start = chunk_end;
+            chunk_end += chunk_length;
+        }
+
+        // last chunk
+        let new_rec = emit_new_maf_rec(&rec, chunk_start, block_length)?;
+        mafwtr.write_record(&new_rec)?;
+    }
+    Ok(())
+}
+
+// emit new maf rec
+fn emit_new_maf_rec(
+    rec: &MAFRecord,
+    chunk_start: u64,
+    chunk_end: u64,
+) -> Result<MAFRecord, WGAError> {
+    let mut new_rec = MAFRecord {
+        score: rec.score,
+        slines: vec![],
+    };
+    for sline in &rec.slines {
+        let new_seq = &sline.seq[chunk_start as usize..chunk_end as usize];
+        let new_sline = MAFSLine {
+            mode: 's',
+            name: sline.name.clone(),
+            start: sline.start + chunk_start,
+            align_size: recount_align_size(new_seq),
+            strand: sline.strand,
+            size: sline.size,
+            seq: new_seq.to_string(),
+        };
+        new_rec.slines.push(new_sline);
+    }
+    Ok(new_rec)
+}
